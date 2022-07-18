05140526cddb427f3457dc13bc6324b5403e71c1
zergon321 <maximgradan@gmail.com>
2022-07-18 21:38:23 +0300

    Comments added.

--- node.go
+++ node.go
@@ -4,6 +4,8 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// node is the representation
+// of the AVL tree node.
 type node[TKey constraints.Ordered, TValue any] struct {
 	height int
 	left   *node[TKey, TValue]
@@ -12,6 +14,8 @@ type node[TKey constraints.Ordered, TValue any] struct {
 	value  TValue
 }
 
+// getHeight returns the level of
+// the node in the AVL tree.
 func (nd *node[TKey, TValue]) getHeight() int {
 	if nd == nil {
 		return 0
@@ -20,10 +24,14 @@ func (nd *node[TKey, TValue]) getHeight() int {
 	return nd.height
 }
 
+// balanceFactor returns the difference
+// between subtrees heights.
 func (nd *node[TKey, TValue]) balanceFactor() int {
 	return nd.right.getHeight() - nd.left.getHeight()
 }
 
+// fixHeight assigns a correct
+// height to the node.
 func (nd *node[TKey, TValue]) fixHeight() {
 	leftHeight := nd.left.getHeight()
 	rightHeight := nd.right.getHeight()
@@ -35,6 +43,8 @@ func (nd *node[TKey, TValue]) fixHeight() {
 	}
 }
 
+// rotateRight performs the right
+// rotation in order to balance the tree.
 func (nd *node[TKey, TValue]) rotateRight() *node[TKey, TValue] {
 	descendant := nd.left
 	nd.left = descendant.right
@@ -46,6 +56,8 @@ func (nd *node[TKey, TValue]) rotateRight() *node[TKey, TValue] {
 	return descendant
 }
 
+// rotateLeft performs the left
+// rotation in order to balance the tree.
 func (nd *node[TKey, TValue]) rotateLeft() *node[TKey, TValue] {
 	descendant := nd.right
 	nd.right = descendant.left
@@ -57,6 +69,8 @@ func (nd *node[TKey, TValue]) rotateLeft() *node[TKey, TValue] {
 	return descendant
 }
 
+// balance performs rotations in order
+// to balance the AVL tree.
 func (nd *node[TKey, TValue]) balance() *node[TKey, TValue] {
 	nd.fixHeight()
 
@@ -79,6 +93,74 @@ func (nd *node[TKey, TValue]) balance() *node[TKey, TValue] {
 	return nd
 }
 
+// search looks for the node with the
+// specified key and returns its value.
+//
+// The second returned value indicates
+// of the key exists in the AVL tree.
+func (nd *node[TKey, TValue]) search(key TKey) (TValue, bool) {
+	var zero TValue
+
+	if nd == nil {
+		return zero, false
+	}
+
+	if nd.key == key {
+		return nd.value, true
+	}
+
+	if key < nd.key {
+		if nd.left == nil {
+			return zero, false
+		}
+
+		return nd.left.search(key)
+	}
+
+	if key > nd.key {
+		if nd.right == nil {
+			return zero, false
+		}
+
+		return nd.right.search(key)
+	}
+
+	return zero, false
+}
+
+// traverse traverses the tree performing
+// the 'visit' function on each node. The
+// order is depth-first.
+func (nd *node[TKey, TValue]) traverse(visit func(currentKey TKey, currentValue TValue) error) error {
+	if nd.left != nil {
+		err := nd.left.traverse(visit)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if nd != nil {
+		err := visit(nd.key, nd.value)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if nd.right != nil {
+		err := nd.right.traverse(visit)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// insert a value by the key in the
+// tree (overwrites the previous value).
 func (nd *node[TKey, TValue]) insert(key TKey, value TValue) *node[TKey, TValue] {
 	if nd == nil {
 		return &node[TKey, TValue]{
@@ -96,6 +178,8 @@ func (nd *node[TKey, TValue]) insert(key TKey, value TValue) *node[TKey, TValue]
 	return nd.balance()
 }
 
+// findMin returns the node with
+// the minimal value of the tree.
 func (nd *node[TKey, TValue]) findMin() *node[TKey, TValue] {
 	if nd.left != nil {
 		nd.left.findMin()
@@ -104,6 +188,8 @@ func (nd *node[TKey, TValue]) findMin() *node[TKey, TValue] {
 	return nd
 }
 
+// removeMin removes the node with
+// the minimal value of the tree.
 func (nd *node[TKey, TValue]) removeMin() *node[TKey, TValue] {
 	if nd.left == nil {
 		return nd.right
@@ -114,6 +200,8 @@ func (nd *node[TKey, TValue]) removeMin() *node[TKey, TValue] {
 	return nd.balance()
 }
 
+// remove removes the node with
+// the specified key from the tree.
 func (nd *node[TKey, TValue]) remove(key TKey) *node[TKey, TValue] {
 	if nd == nil {
 		return nil
--- tree.go
+++ tree.go
@@ -2,6 +2,38 @@ package avl
 
 import "golang.org/x/exp/constraints"
 
+// Tree is the implementation of
+// the AVL tree data structure.
 type Tree[TKey constraints.Ordered, TValue any] struct {
 	root *node[TKey, TValue]
+}
+
+// Insert places the key with the value
+// in the tree (overwrites the previous
+// value for the key if it exists).
+func (tree *Tree[TKey, TValue]) Insert(key TKey, value TValue) {
+	tree.root = tree.root.insert(key, value)
+}
+
+// Search searches for the key and returns its
+// value. The second returned value indicates
+// if the node with the key exists in the tree.
+func (tree *Tree[TKey, TValue]) Search(key TKey) (TValue, bool) {
+	return tree.root.search(key)
+}
+
+// Traverse traverses the tree from the
+// min key node to the max key node.
+func (tree *Tree[TKey, TValue]) Traverse(visit func(currentKey TKey, currentValue TValue) error) error {
+	return tree.root.traverse(visit)
+}
+
+// Remove removes the node with the specified key.
+func (tree *Tree[TKey, TValue]) Remove(key TKey) {
+	tree.root = tree.root.remove(key)
+}
+
+// NewTree creates a new empty tree.
+func NewTree[TKey constraints.Ordered, TValue any]() *Tree[TKey, TValue] {
+	return &Tree[TKey, TValue]{}
 }
